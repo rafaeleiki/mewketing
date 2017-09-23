ace57bed96e21739ed6d175b395097079ffad8aa
Valarini <guilherme.a.valarini@gmail.com>
2017-09-22 23:35:09 -0300

    Groups receiver (#4)
    * Added the group receivers table
    
    * Fix groups receivers db
    
    * Model for the group_receiver created
    
    * Fixed group receiver
    
    * Removed drop table from migration

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :sender
-  has_many :group_receiver, :dependent => :delete_all
+  has_many :receivers, :through => :group_receivers
+  has_many :group_receivers, :dependent => :delete_all
 end
