8f2f0a4e1bed97bc7cfcf130a6660babfb0bd5e1
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2017-09-28 13:44:17 -0300

    Template can now be really destroyed

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,14 +2,4 @@ class Template < ApplicationRecord
   belongs_to :sender
   validates :title, presence: true
   validates :title, uniqueness: true
-
-  # Scopes
-  scope :active, -> {where(enabled: true)}
-  scope :inactive, -> {where(enabled: false)}
-
-  # Include the management of the enabled flag
-  def destroy
-    self.enabled = false
-    save
-  end
 end
