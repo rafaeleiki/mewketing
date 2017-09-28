56ba04acb0e1b55bc787f24caa04a38f2efaf1a9
Rafael <rafaeleiki@yahoo.com.br>
2017-09-28 11:57:48 -0300

    Fixes sender password information

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -2,4 +2,13 @@ class Sender < ApplicationRecord
   belongs_to :client
   has_many :groups
   has_secure_password
+
+  def validate_new_password(old_password, new_password, confirm_password)
+    if persisted?
+      valid_password = authenticate(old_password)
+      errors.add(:password, message: 'Invalid password') unless valid_password
+    end
+
+    errors.add(:password, message: 'Invalid password confirmation') if new_password != confirm_password
+  end
 end
