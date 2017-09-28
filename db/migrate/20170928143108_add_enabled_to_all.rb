97e74d1651472aea3bb9521b1c02b48523d181db
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2017-09-28 12:54:47 -0300

    Added default value of enabled to true

--- db/migrate/20170928143108_add_enabled_to_all.rb
+++ db/migrate/20170928143108_add_enabled_to_all.rb
@@ -1,10 +1,10 @@
 class AddEnabledToAll < ActiveRecord::Migration[5.1]
   def change
-    add_column :clients, :enabled, :boolean
-    add_column :senders, :enabled, :boolean
-    add_column :receivers, :enabled, :boolean
-    add_column :groups, :enabled, :boolean
-    add_column :templates, :enabled, :boolean
-    add_column :emails, :enabled, :boolean
+    add_column :clients, :enabled, :boolean, :default => true
+    add_column :senders, :enabled, :boolean, :default => true
+    add_column :receivers, :enabled, :boolean, :default => true
+    add_column :groups, :enabled, :boolean, :default => true
+    add_column :templates, :enabled, :boolean, :default => true
+    add_column :emails, :enabled, :boolean, :default => true
   end
 end
