1bde4bb813e93d36fa7e6ee7adf22c05f74fe138
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2017-09-28 13:04:00 -0300

    Removed all delete all from models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,4 @@
 class Client < ApplicationRecord
-  has_many :senders, :dependent => :delete_all
-
   # Include the management of the enabled flag
   def destroy
     self.enabled = false
--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -1,7 +1,6 @@
 class Receiver < ApplicationRecord
   belongs_to :sender
   has_many :groups, :through => :group_receivers
-  has_many :group_receivers, :dependent => :delete_all
 
   # Include the management of the enabled flag
   def destroy
