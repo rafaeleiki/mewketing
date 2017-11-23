f80a67c5015460f4e4a502d6374df612ef6574ff
Guilherme Valarini <guilherme.a.valarini@gmail.com>
2017-11-23 00:09:26 -0200

    Apis implementation (#17)
    * Added the group receivers table
    
    * Fix groups receivers db
    
    * Drop table removed from migrate
    
    * Seeds updated with more examples
    
    * Renamed email API file
    
    * Added groups_receivers models
    
    * Added group api
    
    * renamed email api file
    
    * Added group_api again
    
    * Created API to group module
    
    * Fixed get call. Fixed some returns to calls.
    
    * Removed CRUD template
    
    * Added admin scope to sender model.
    
    * Added sender api service as a mounted service.
    
    * Added active scope to activation process.
    
    * Added removable scope to sender model
    
    * Chaged get all params
    
    * Changed update error message.
    
    * Fixed group get all
    
    * Added sender API
    
    * Added receiver API
    
    * Added active scope to sender when authorizing
    
    * Added template API
    
    * Fixed template get
    
    * Fixed sending an email at schedule time
    
    * Schema updated
    
    * Fixed email sent right at schedule time
    
    * Added email API
    
    * Return messages improved.

---
+++ app/api/services/group_api.rb
@@ -0,0 +1,131 @@
+module Services
+  class GroupAPI < Grape::API
+    version 'v1', using: :header, vendor: 'group'
+    content_type :json, 'application/json;charset=utf-8'
+
+    # Actions to be performed before all api responses
+    before {
+      authorize(params[:sender_email], params[:sender_pass])
+      get_params(params)
+    }
+
+    # Helpers
+    helpers do
+      params :credentials do
+        requires :sender_email, type: String, desc: 'Sender email for authentication'
+        requires :sender_pass, type: String, desc: 'Sender pass for authentication'
+      end
+
+      def authorize(email, pass)
+        @user = Sender.active.find_by_email(email)
+        error!("Unauthorized") if !(@user && @user.authenticate(pass))
+      end
+
+      def get_params(params)
+        @q_params = {sender: @user}
+        params.except(:sender_email, :sender_pass, :original_name).each_pair do |key, value|
+          @q_params[key] = value unless value.nil?
+        end
+      end
+    end
+
+    # API call souvers
+    resource :group do
+      # CRUD - DB Management
+      desc 'Add a new group'
+      params do
+        use :credentials
+        requires :name, type: String, desc: 'Name of the new group'
+        requires :private, type: Boolean, desc: 'Name of the new group'
+      end
+      post :add do
+        g = Group.active.find_by(sender: @user, name: params[:name])
+        error!("Group already created") if !g.nil?
+        g = Group.create(@q_params)
+        error!("Internal error") if g.nil?
+        return {"status":"Group created"}
+      end
+
+      desc 'Read some group'
+      params do
+        use :credentials
+        optional :name, type: String, desc: 'Name of the new group'
+        optional :private, type: Boolean, desc: 'Name of the new group'
+      end
+      post :get do
+        Group.active.where(@q_params)
+      end
+
+      desc 'Read all groups'
+      params do
+        use :credentials
+      end
+      post :get_all do
+        Group.active.where(sender: @user).all
+      end
+
+      desc 'Update one group'
+      params do
+        use :credentials
+        requires :original_name, type: String, desc: 'Name of the group'
+        optional :name, type: String, desc: 'Name of the group'
+        optional :private, type: Boolean, desc: 'Name of the group'
+      end
+      post :update do
+        g = Group.active.find_by(sender: @user, name: params[:original_name])
+        error!("Group not found") if g.nil?
+        gn = Group.active.find_by(sender: @user, name: params[:name])
+        error!("There is already a group with this name") if !gn.nil?
+        g.update(@q_params)
+        return {"status":"Group updated"}
+      end
+
+      desc 'Remove one '
+      params do
+        use :credentials
+        requires :name, type: String, desc: 'Name of the new group'
+      end
+      post :remove do
+        g = Group.active.find_by(@q_params)
+        error!("Group not found") if g.nil?
+        error!("Internal error") if !g.destroy
+        return {"status":"Group removed"}
+      end
+
+      # Receivers management
+      desc 'Add a receiver to a group'
+      params do
+        use :credentials, desc: "Credentials"
+        requires :group_name, type: String, desc: 'Name of the group to add the receiver'
+        requires :receiver_email, type: String, desc: 'Email of the receiver to be added'
+      end
+      post :add_receiver do
+        g = Group.active.find_by(name: params[:group_name])
+        r = Receiver.active.find_by(email: params[:receiver_email])
+        error!("Group not found") if g.nil?
+        error!("Receiver not found") if r.nil?
+        error!("Receiver already in group") if !GroupReceiver.find_by(group: g, receiver: r).nil?
+        gr = GroupReceiver.create(group: g, receiver: r)
+        error!("Internal error") if gr.nil?
+        return {"status":"Receiver added to group"}
+      end
+
+      desc 'Remove a receiver from a group'
+      params do
+        use :credentials, desc: "Credentials"
+        requires :group_name, type: String, desc: 'Name of the group'
+        requires :receiver_email, type: String, desc: 'Email of the receiver to be removed'
+      end
+      post :remove_receiver do
+        g = Group.active.find_by(name: params[:group_name])
+        r = Receiver.active.find_by(email: params[:receiver_email])
+        error!("Group not found") if g.nil?
+        error!("Receiver not found") if r.nil?
+        gr = GroupReceiver.find_by(group: g, receiver: r)
+        error!("Receiver not inside group") if gr.nil?
+        error!("Internal error") if !gr.destroy
+        return {"status":"Receiver removed from group"}
+      end
+    end
+  end
+end
---
+++ app/api/services/template_api.rb
@@ -0,0 +1,95 @@
+module Services
+  class TemplateAPI < Grape::API
+    version 'v1', using: :header, vendor: 'template'
+    content_type :json, 'application/json;charset=utf-8'
+
+    # Actions to be performed before all api responses
+    before {
+      authorize(params[:sender_email], params[:sender_pass])
+      get_params(params)
+    }
+
+    # Helpers
+    helpers do
+      params :credentials do
+        requires :sender_email, type: String, desc: 'Sender email for authentication'
+        requires :sender_pass, type: String, desc: 'Sender pass for authentication'
+      end
+
+      def authorize(email, pass)
+        @user = Sender.active.find_by_email(email)
+        error!("Unauthorized") if !(@user && @user.authenticate(pass))
+      end
+
+      def get_params(params)
+        @q_params = {sender: @user}
+        params.except(:sender_email, :sender_pass, :original_title).each_pair do |key, value|
+          @q_params[key] = value unless value.nil?
+        end
+      end
+    end
+
+    # API call souvers
+    resource :template do
+      # CRUD - DB Management
+      desc 'Add a new template'
+      params do
+        use :credentials
+        requires :title, type: String, desc: 'Title of the new template'
+        requires :body, type: String, desc: 'Body of the new template'
+      end
+      post :add do
+        t = Template.find_by(sender: @user, title: params[:title])
+        error!("Template already created") if !t.nil?
+        t = Template.create(@q_params)
+        error!("Internal error") if t.nil?
+        return {"status":"Template created"}
+      end
+
+      desc 'Read some templates'
+      params do
+        use :credentials
+        optional :title, type: String, desc: 'Title of the template'
+      end
+      post :get do
+        Template.where(@q_params)
+      end
+
+      desc 'Read all templates'
+      params do
+        use :credentials
+      end
+      post :get_all do
+        Template.where(sender: @user).all
+      end
+
+      desc 'Update one template'
+      params do
+        use :credentials
+        requires :original_title, type: String, desc: 'Original title of the template'
+        optional :title, type: String, desc: 'Title of the template'
+        optional :body, type: String, desc: 'Body of the template'
+      end
+      post :update do
+        t = Template.find_by(sender: @user, title: params[:original_title])
+        error!("Template not found") if t.nil?
+        rn = Template.find_by(sender: @user, title: params[:title])
+        error!("There is already a template with this title") if !rn.nil?
+        t.update(@q_params)
+        return {"status":"Template updated"}
+      end
+
+      desc 'Remove one '
+      params do
+        use :credentials
+        requires :title, type: String, desc: 'Title of the template'
+      end
+      post :remove do
+        g = Template.find_by(@q_params)
+        error!("Template not found") if g.nil?
+        error!("Internal error") if !g.destroy
+        return {"status":"Template removed"}
+      end
+    end
+  end
+end
